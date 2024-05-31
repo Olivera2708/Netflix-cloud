d6b51953ccf826e222038635b51a838ee0ccce12
OgnjenGligoric <gligoric383@gmail.com>
2024-05-31 21:12:16 +0200

    create dynamo db movie table

--- project/project/project_stack.py
+++ project/project/project_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     aws_s3 as s3, 
-    Stack
+    Stack,
+    aws_dynamodb as dynamodb
 )
 from constructs import Construct
 
@@ -9,8 +10,19 @@ class ProjectStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        s3.Bucket(
+        movies_bucket = s3.Bucket(
             self, 
             id="movies-team3", 
             bucket_name="movies-team3"
             )
+
+        movies_table = dynamodb.Table(
+            self, "movies-table-team3",
+            table_name="movies-table-team3",
+            partition_key=dynamodb.Attribute(
+                name="name",
+                type=dynamodb.AttributeType.STRING
+            ),
+            read_capacity=1,
+            write_capacity=1
+        )
