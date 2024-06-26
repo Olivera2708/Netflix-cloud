f3e7bff41d02e9737efc92a4fa99449f1e3f261f
Milos Bojanic <mbojanic53@gmail.com>
2024-06-26 18:24:31 +0200

    Lambda for calculating score based on subscription

--- project/calculate_subscription/calculate_subscription.py
+++ project/calculate_subscription/calculate_subscription.py
@@ -1,3 +1,14 @@
 def calculate_subscription(event, context):
+    subscribed_actiors = set(event['subscriptions']['actors'])
+    subscribed_genres = set(event['subscriptions']['genres'])
+    subscribed_directors = set(event['subscriptions']['directors'])
 
-    return event
+    movie_actiors = set(event['actors'])
+    movie_genres = set(event['genres'])
+    movie_directors = set(event['directors'])
+
+    subscription_score = (len(subscribed_actiors & movie_actiors) + len(subscribed_directors & movie_directors) + len(subscribed_genres & movie_genres)) * 10
+    
+    return {
+        'subscription_score': subscription_score
+    }
